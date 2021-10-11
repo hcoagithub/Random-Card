2542fc7930dacaccf333ad2c03b5d89213edc9ad
hcoagithub <hectorcoa.14@gmail.com>
2021-10-11 02:20:05 +0000

    Este es mi random card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,53 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ["♦", "♥", "♠", "♣"];
+  let numCard = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2"
+  ];
+
+  const iconoSuperior = document.querySelector("#iconoSuperior");
+  const iconoInferior = document.querySelector("#iconoInferior");
+  const numero = document.querySelector("#numero");
+  const carta = document.querySelector(".card");
+
+  numero.innerHTML = numCard[numAleatorio(numCard)];
+
+  let paloAleatorio = palo[numAleatorio(palo)];
+
+  if (paloAleatorio === "♦" || paloAleatorio === "♥") {
+    if (carta.classList.contains("black")) {
+      carta.classList.remove("black");
+    } else {
+      carta.classList.add("red");
+      iconoSuperior.innerHTML = paloAleatorio;
+      iconoInferior.innerHTML = iconoSuperior.innerHTML;
+    }
+  } else if (paloAleatorio === "♠" || paloAleatorio === "♣") {
+    if (carta.classList.contains("red")) {
+      carta.classList.remove("red");
+    } else {
+      carta.classList.add("black");
+      iconoSuperior.innerHTML = paloAleatorio;
+      iconoInferior.innerHTML = iconoSuperior.innerHTML;
+    }
+  }
 };
+
+function numAleatorio(arr) {
+  let num = Math.floor(Math.random() * arr.length);
+
+  return num;
+}
